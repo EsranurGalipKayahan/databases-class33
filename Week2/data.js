88e3389187af6610c21d8677b594ef96fc13cfd8
EsranurGalipKayahan <61011249+EsranurGalipKayahan@users.noreply.github.com>
2021-10-16 14:32:27 +0200

    deneme

---
+++ Week2/data.js
@@ -0,0 +1,318 @@
+export const authors = [
+    {
+        author_no: 1,
+        author_name: "Esra",
+        university: "Ege",
+        date_of_birth: "1990-11-22",
+        h_index: 3,
+        gender: 'f'
+    },
+    {
+        author_no: 2,
+        author_name: "Sam",
+        university: "Groningen",
+        date_of_birth: "1989-11-11",
+        h_index: null,
+        gender: 'm',
+        mentor: 1
+    },
+    {
+        author_no: 3,
+        author_name: "Lisa",
+        university: "Leiden",
+        date_of_birth: "1993-12-07",
+        h_index: 6,
+        gender: 'f',
+        mentor: 2
+    },
+    {
+        author_no: 4,
+        author_name: "Cuma",
+        university: "Sakarya",
+        date_of_birth: "1991-08-30",
+        h_index: 2,
+        gender: 'f',
+    },
+    {
+      author_no: 5,
+      author_name: "Sara",
+      university: "Erasmus",
+      date_of_birth: "1998-11-16",
+      h_index: 7,
+      gender: 'f',
+      mentor: 1
+  },
+  {
+    author_no: 6,
+    author_name: "Grietje",
+    university: "Utrecht",
+    date_of_birth: "1965-10-09",
+    h_index: null,
+    gender: 'f',
+    mentor: null
+},
+{
+  author_no: 7,
+  author_name: "Jacob",
+  university: "Delft",
+  date_of_birth: "1956-10-09",
+  h_index: 2,
+  gender: 'm',
+  mentor: null
+},
+{
+  author_no: 8,
+  author_name: "Adam",
+  university: "Utrecht",
+  date_of_birth: "1985-10-09",
+  h_index: 5,
+  gender: 'm',
+  mentor: 3
+},
+{
+  author_no: 9,
+  author_name: "Ahmed",
+  university: "Technic",
+  date_of_birth: "1991-01-01",
+  h_index: 6,
+  gender: 'f',
+  mentor: 4
+},
+{
+  author_no: 10,
+  author_name: "Joseph",
+  university: "MIT",
+  date_of_birth: "1954-10-09",
+  h_index: 1,
+  gender: 'f',
+  mentor: null
+},
+{
+  author_no: 11,
+  author_name: "Fatima",
+  university: "MIT",
+  date_of_birth: "1975-07-09",
+  h_index: 3,
+  gender: 'f',
+  mentor: 10
+},
+{
+  author_no: 12,
+  author_name: "Alex",
+  university: "MIT",
+  date_of_birth: "1985-03-02",
+  h_index: 5,
+  gender: 'm',
+  mentor: 10
+},
+{
+  author_no: 13,
+  author_name: "Saad",
+  university: "Ege",
+  date_of_birth: "1985-02-02",
+  h_index: 4,
+  gender: 'm',
+  mentor: 5
+},
+{
+  author_no: 14,
+  author_name: "Nesibe",
+  university: "Konya",
+  date_of_birth: "1985-02-20",
+  h_index: 4,
+  gender: 'f',
+  mentor: 6
+},
+{
+  author_no: 15,
+  author_name: "Jasmin",
+  university: "Ohio",
+  date_of_birth: "1983-03-02",
+  h_index: 3,
+  gender: 'f',
+  mentor: 5
+},
+  ];
+
+  export const papers = [
+    {
+      paper_id:1001,
+      paper_title: "Why are we in the World?",
+      conference:"ISCS",
+      publish_date: "2019-10-10"
+    },
+    {
+      paper_id:1002,
+      paper_title: "Why?",
+      conference:"ESCS",
+      publish_date: "2011-10-10"
+    },
+    {
+      paper_id:1003,
+      paper_title: "New Consepts in ES6",
+      conference:"ISCSE",
+      publish_date: "2020-10-10"
+    },
+    {
+      paper_id:1004,
+      paper_title: "What is the World?",
+      conference:"ISCSE",
+      publish_date: "2020-10-10"
+    },
+    {
+      paper_id:1005,
+      paper_title: "Harmony Search Algorithms",
+      conference:"ISE",
+      publish_date: "2012-10-10"
+    },
+    {
+      paper_id:1006,
+      paper_title: "Gender in the Society",
+      conference:"MESC",
+      publish_date: "2021-10-10"
+    },
+    {
+      paper_id:1007,
+      paper_title: "Reasons of Economical Crisis",
+      conference:"MESC",
+      publish_date: "2019-12-10"
+    },
+    {
+      paper_id:1008,
+      paper_title: "AI in E-Commerce",
+      conference:"ISCSE",
+      publish_date: "2012-10-10"
+    },
+    {
+      paper_id:1009,
+      paper_title: "Importance of food",
+      conference:"ISCS",
+      publish_date: "2013-10-10"
+    },
+    {
+      paper_id:1010,
+      paper_title: "The balance in the marriage",
+      conference:"ISE",
+      publish_date: "2019-08-10"
+    },
+    {
+      paper_id:1011,
+      paper_title: "The purpose of the live",
+      conference:"ISC",
+      publish_date: "2015-10-10"
+    },
+    {
+      paper_id:1012,
+      paper_title: "Who am I?",
+      conference:"IS",
+      publish_date: "2000-10-10"
+    },
+    {
+      paper_id:1013,
+      paper_title: "The charachteristic of weakness",
+      conference:"ISCM",
+      publish_date: "2018-10-10"
+    },
+    {
+      paper_id:1014,
+      paper_title: "Minimizing delay in FTS",
+      conference:"ISCE",
+      publish_date: "2018-10-10"
+    },
+    {
+      paper_id:1015,
+      paper_title: "Never Give Up!",
+      conference:"ISCE",
+      publish_date: "2019-10-10"
+    },
+    {
+      paper_id:1016,
+      paper_title: "The dubbel personalities",
+      conference:"IMS",
+      publish_date: "2017-10-10"
+    },
+    {
+      paper_id:1017,
+      paper_title: "The new approach for foreign languages",
+      conference:"ISM",
+      publish_date: "2015-10-10"
+    },
+    {
+      paper_id:1018,
+      paper_title: "The effects of Social Medias over the Society",
+      conference:"ISCM",
+      publish_date: "2015-11-10"
+    },
+    {
+      paper_id:1019,
+      paper_title: "The investment",
+      conference:"IER",
+      publish_date: "2018-10-10"
+    },
+    {
+      paper_id:1020,
+      paper_title: "The fire",
+      conference:"ISC",
+      publish_date: "2018-10-10"
+    },
+    {
+      paper_id:1021,
+      paper_title: "The Woman",
+      conference:"ISCM",
+      publish_date: "2012-10-10"
+    },
+    {
+      paper_id:1022,
+      paper_title: "The Man",
+      conference:"ISCM",
+      publish_date: "2012-10-10"
+    },
+    {
+      paper_id:1023,
+      paper_title: "The Kind",
+      conference:"ISCM",
+      publish_date: "2012-10-10"
+    },
+    {
+      paper_id:1024,
+      paper_title: "The Family",
+      conference:"ISCM",
+      publish_date: "2012-10-10"
+    },
+    {
+      paper_id:1025,
+      paper_title: "The Simple Education",
+      conference:"ISM",
+      publish_date: "2015-10-10"
+    },
+    {
+      paper_id:1026,
+      paper_title: "New Historical Researches",
+      conference:"IS",
+      publish_date: "2021-10-10"
+    },
+    {
+      paper_id:1027,
+      paper_title: "The Koningdom",
+      conference:"IS",
+      publish_date: "2021-10-10"
+    },
+    {
+      paper_id:1028,
+      paper_title: "The traffic in Istanbul",
+      conference:"IM",
+      publish_date: "2021-10-10"
+    },
+    {
+      paper_id:1029,
+      paper_title: "The History of Izmir",
+      conference:"ISM",
+      publish_date: "2012-10-10"
+    },
+    {
+      paper_id:1030,
+      paper_title: "The Amazon",
+      conference:"ISCM",
+      publish_date: "2003-10-10"
+    },
+  ];
