66d9be4bf6c130a1850a8dd993415ce01839c1c2
EsranurGalipKayahan <61011249+EsranurGalipKayahan@users.noreply.github.com>
2021-10-17 11:15:26 +0200

    Revised according to review

--- Week1/Exercise1/utils/sqls.js
+++ Week1/Exercise1/utils/sqls.js
@@ -13,13 +13,13 @@ export const showTableDefinition = (tableName) => {
   return `DESCRIBE ${tableName}`;
 };
 export const create_invitee_table = () => {
-  return "CREATE TABLE Invitee(invitee_no INT, invitee_name VARCHAR(50), invited_by VARCHAR(50),PRIMARY KEY(invitee_no))";
+  return "CREATE TABLE Invitee(invitee_no INT, invitee_name VARCHAR(50) NOT NULL, invited_by VARCHAR(50) NOT NULL,PRIMARY KEY(invitee_no))";
 };
 export const create_room_table = () => {
-  return "CREATE TABLE Room(room_no INT, room_name VARCHAR(50), floor_number INT, PRIMARY KEY(room_no))";
+  return "CREATE TABLE Room(room_no INT, room_name VARCHAR(50) NOT NULL, floor_number INT NOT NULL, PRIMARY KEY(room_no))";
 };
 export const create_meeting_table = () => {
-  return "CREATE TABLE Meeting(meeting_no INT, meeting_title VARCHAR(50), starting_time DATETIME, ending_time DATETIME,room_no INT, PRIMARY KEY(meeting_no))";
+  return "CREATE TABLE Meeting(meeting_no INT, meeting_title VARCHAR(50) NOT NULL, starting_time DATETIME NOT NULL, ending_time DATETIME NOT NULL,room_no INT, PRIMARY KEY(meeting_no))";
 };
 export const add_foreign_key = () => {
   return "ALTER TABLE Meeting ADD CONSTRAINT fk FOREIGN KEY (room_no) REFERENCES Room(room_no)";
